b7a84762e58d7ad918d6992efc4c75714ebb33f9
JorgeErnestoSantos <jorgeernesto.santos05031774@gmail.com>
2025-06-16 01:23:37 -0400

    Actualizar Load_data.py

--- Load_data.py
+++ Load_data.py
@@ -10,16 +10,15 @@ class Pelicula:
         self.año_estreno = año_estreno
         self.duracion = duracion
 
-ruta_archivo = "title.basic.tsv.gz"
-BD = p.read_csv(ruta_archivo , sep='\t', compression='gzip' , low_memory= False)
-BD_peliculas = BD[BD['titleType'] == 'movie'].copy()
-
-BD_peliculas['startYear'] = p.to_numeric(BD_peliculas['startYear'] , errors= 'coerce')
-BD_peliculas['runtimeMinutes'] = p.to_numeric(BD_peliculas['runtimeMinutes'] , errors= 'coerce')
-
+ruta_archivo = "peliculas.tsv"
+BD = p.read_csv(ruta_archivo , sep='\t', low_memory= False)
+Bd = BD.to_dict("records")
+for i in Bd:
+    i["startYear"] = p.to_numeric(i["startYear"] , errors= 'coerce')
+    i["runtimeMinutes"] = p.to_numeric(i["runtimeMinutes"] , errors= 'coerce')
 lista_peliculas = []
-for _, row in BD_peliculas.iterrows():
-    pelicula = Pelicula (nombre=row['primaryTitle'] , nombre_original=row['originalTitle'] , generos=row['genres'].split(',') if p.notna(row['genres']) else [], año_estreno= int(row['startYear'] if p.notna(row['startYear']) else None) , duracion= int(row['runtimeMinutes']) if p.notna(row['runtimeMinutes']) else None)
+for i in Bd:
+    pelicula = Pelicula (nombre=i["primaryTitle"] , nombre_original=i["originalTitle"] , generos=i["genres"].split(',') if p.notna(i["genres"]) else [], año_estreno= int(i["startYear"]) if p.notna(i["startYear"]) else None , duracion= int(i["runtimeMinutes"]) if p.notna(i["runtimeMinutes"]) else None)
     lista_peliculas.append(pelicula)
 
 
@@ -32,13 +31,15 @@ class Serie:
         self.año_inicio = año_inicio
         self.año_fin = año_fin
 
-BD_series = BD[BD['titleType'] == 'tvSeries'].copy()
-BD_series['startYear'] = p.to_numeric(BD_series['startYear'], errors= 'coerce')
-BD_series['endYear'] = p.to_numeric(BD_series['endYear'], errors='coerce')
-
+ruta_archivo = "series.tsv"
+BD = p.read_csv(ruta_archivo, sep="\t", low_memory= False)
+Bd = BD.to_dict("records")
+for i in Bd:
+    i["startYear"] = p.to_numeric(i["startYear"], errors= 'coerce')
+    i["endYear"] = p.to_numeric(i["endYear"], errors='coerce')
 lista_series = []
-for _,row in BD_series:
-    serie = Serie (nombre= row['primaryTitle'], nombre_original= row['originalTitle'], generos= row['genre'] if p.notna(row['genre']) else [] , año_inicio= row['startYear'] if p.notna(row['startYear']) else None , año_fin= row['endYear'] if p.notna(row['endYear']) else None)
-    lista_series.append(serie)
+for i in Bd:
+    serie = Serie (nombre= i["primaryTitle"], nombre_original= i["originalTitle"], generos= i["genres"] if p.notna(i["genres"]) else [] , año_inicio= int(i["startYear"]) if p.notna(i["startYear"]) else None , año_fin= int(i["endYear"]) if p.notna(i["endYear"]) else None)
+    lista_series.append(serie) 
 
  
