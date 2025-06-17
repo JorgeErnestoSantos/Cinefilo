1f1e292e99cc3a2bd2040cc6bc6d7d56dda6bd6d
JorgeErnestoSantos <jorgeernesto.santos05031774@gmail.com>
2025-06-16 23:50:29 -0400

    Actualizar App.py

--- App.py
+++ App.py
@@ -14,20 +14,20 @@ if opcion == "Series":
     opc = st.radio ("¿Qué tipo de analisis quieres hacer?", options=["Géneros más populares en X año", "Año con más series finalizadas", "Año con más series estrenadas"], horizontal= True)
     if opc == "Géneros más populares en X año":
         año_a = st.slider ("Año", 1894, 2025)
-        print(bd_series.graf_genero_año(año_a))
+        st.pyplot(bd_series.graf_genero_año(año_a))
     elif opc == "Año con más series estrenadas":
-        print(bd_series.año_emision)
+        st.write(bd_series.año_emision)
     else:
-        print(bd_series.año_finalizacion)
+        st.write(bd_series.año_finalizacion)
 else:
     bd_pelis = Análisis_p.analizador_pelis(Load_data.lista_peliculas)
     opt = st.radio("¿Qué tipo de análisis te gustaría hacer?", options=["Géneros más populares en X año", "Película más duradera en X año", "Película menos duradera en X año"], horizontal= True)
     if opt == "Géneros más populares en X año":
         año_p = st.slider("Año", 1888 , 2025)
-        print(bd_pelis.graf_genero_año(año_p))
+        st.pyplot(bd_pelis.graf_genero_año(año_p))
     elif opt == "Película más duradera en X año":
         año_b = st.slider("Año", 1888, 2025)
-        print(bd_pelis.mayor_duración(año_b))
+        st.write(bd_pelis.mayor_duración(año_b))
     else:
         año_c = st.slider("Año", 1888, 2025)
-        print(bd_pelis.menor_duraciom(año_c))
+        st.write(bd_pelis.menor_duraciom(año_c))
